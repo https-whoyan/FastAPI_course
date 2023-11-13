6fcd3fbd720aaf99c4629df5f3bb8c779cce8703
Nevezhin Yan <yaniknezhin@gmail.com>
2023-11-14 01:03:16 +0600

    Create Auth System

---
+++ auth/schemas.py
@@ -0,0 +1,28 @@
+import uuid
+
+from fastapi_users import schemas
+from typing import Optional
+from pydantic import ConfigDict
+from pydantic.version import VERSION as PYDANTIC_VERSION
+
+PYDANTIC_V2 = PYDANTIC_VERSION.startswith("2.")
+
+
+class UserRead(schemas.BaseUser[int]):
+    id: int
+    email: str
+    username: str
+    role_id: int
+    is_active: bool = True
+    is_superuser: bool = False
+    is_verified: bool = False
+
+
+class UserCreate(schemas.BaseUserCreate):
+    username: str
+    role_id: int
+    email: str
+    password: str
+    is_active: Optional[bool] = True
+    is_superuser: Optional[bool] = False
+    is_verified: Optional[bool] = False
--- config.py
+++ config.py
@@ -3,8 +3,10 @@ import os
 
 load_dotenv()
 
-DB_HOST=os.environ.get("DB_HOST")
-DB_USER=os.environ.get("DB_USER")
-DB_NAME=os.environ.get("DB_NAME")
-DB_PORT=os.environ.get("DB_PORT")
-DB_PASS=os.environ.get("DB_PASS")
+DB_HOST = os.environ.get("DB_HOST")
+DB_USER = os.environ.get("DB_USER")
+DB_NAME = os.environ.get("DB_NAME")
+DB_PORT = os.environ.get("DB_PORT")
+DB_PASS = os.environ.get("DB_PASS")
+JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
+USER_MANAGER_SECRET_KEY = os.environ.get("USER_MANAGER_SECRET_KEY")
